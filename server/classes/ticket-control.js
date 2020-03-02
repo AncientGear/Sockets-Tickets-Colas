e3d135140ae6d5c65b9569e131424157a5abfda1
SaulTzakumOchoa <cpp.saul@gmail.com>
2020-03-02 00:04:37 -0600

    Showing Ticket File on Web Screen

---
+++ server/classes/ticket-control.js
@@ -0,0 +1,93 @@
+'use strict'
+const fs = require('fs');
+
+class Ticket{
+    constructor(numero, escritorio){
+        this.numero = numero;
+        this.escritorio = escritorio;
+        
+    }
+}
+
+class TicketControl {
+    constructor(){
+        this.ultimo = 0;
+        this.hoy = new Date().getDate();
+        this.tickets = [];
+        this.ultimos4 = [];
+
+        let data = require('../data/data.json');
+
+        if(data.hoy === this.hoy){
+            this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
+        } else{
+            this.reiniciarConteo();
+        }
+    }
+
+    siguienteTicket() {
+        this.ultimo += 1;
+
+        let ticket = new Ticket(this.ultimo, null);
+
+        this.tickets.push(ticket);
+
+        this.guardarArchivo();
+
+        return `Ticket: ${this.ultimo}`
+    }
+
+    getUltimoTicket(){
+        return `Ticket: ${this.ultimo}`;
+    }
+
+    getUltimos4(){
+        return this.ultimos4;
+    }
+
+    atenderTikcet(escritorio){
+        if(this.tickets.length == 0){
+            return 'No hay tickets';
+        }
+        let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+
+        let atender_ticket = new Ticket(numeroTicket, escritorio);;
+        
+        this.ultimos4.unshift(atender_ticket);
+
+        if(this.ultimos4.length > 4){
+            this.ultimos4.splice(-1,1); // Borra el último elemento
+            
+        }
+        
+        this.guardarArchivo();
+
+        return atender_ticket;
+    }
+
+    reiniciarConteo() {
+        this.ultimo = 0;
+        this.guardarArchivo();
+        this.tickets = [];
+        this.ultimos4 = [];
+    }
+
+    guardarArchivo() {
+        let jsonData = {
+            ultimo: this.ultimo,
+            hoy: this.hoy,
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
+        }
+        
+        let jsonDataString = JSON.stringify(jsonData);
+
+        fs.writeFileSync('./server/data/data.json', jsonDataString);
+    }
+}
+
+module.exports = {
+    TicketControl
+}
---
+++ server/sockets/socket.js
@@ -0,0 +1,37 @@
+const { io } = require('../server');
+const {TicketControl} = require('../classes/ticket-control');
+const fs = require('fs');
+
+let ticketControl = new TicketControl();
+io.on('connection', (client) => {
+
+    client.on('siguienteTicket', (data, callback) => {
+        let siguiente = ticketControl.siguienteTicket();
+        
+        callback(siguiente);
+    })
+
+    // Emitir un evento estadoActual
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    })
+
+    client.on('atenderTicket', (data, callback) => {
+        if(!data.escritorio){
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            })
+        }
+
+        let atenderTicket = ticketControl.atenderTikcet(data.escritorio);
+        
+        callback(atenderTicket);
+
+        // Notificar cambios en los ultimos 4
+        
+        
+    })
+});
+
